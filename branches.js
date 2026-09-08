3833fd1f213da91a98fc19c017c87099f4580dd3
managerquicky <manager.quicky@gmail.com>
2026-09-08 12:29:10 +0700

    Create branches.js
    Create branches.js

---
+++ branches.js
@@ -0,0 +1,98 @@
+const branches = [
+{
+name:"QUICKY เสรีไทย29",
+lat:13.7800,
+lng:100.6840,
+map:"https://maps.app.goo.gl/3ZRW6VsVCQiDJy568"
+},
+{
+name:"QUICKY เทพารักษ์",
+lat:13.6400,
+lng:100.6400,
+map:"https://maps.app.goo.gl/Vd9MdzKN8Xr3b7ibA"
+},
+{
+name:"QUICKY ปากน้ำ",
+lat:13.5900,
+lng:100.6000,
+map:"https://maps.app.goo.gl/R7wj3Et1cYPG26Rc9"
+},
+{
+name:"QUICKY บางพลี",
+lat:13.6100,
+lng:100.7300,
+map:"https://maps.app.goo.gl/c3oghqcsS2M2PC9DA"
+},
+{
+name:"QUICKY บางนา",
+lat:13.6700,
+lng:100.6300,
+map:"https://maps.app.goo.gl/VSoon9GZGXN9pTd18"
+},
+{
+name:"QUICKY สุขุมวิท50",
+lat:13.7000,
+lng:100.6000,
+map:"https://maps.app.goo.gl/3XAmHxo3LarqYFKY6"
+},
+{
+name:"QUICKY เลี่ยงเมืองนนทบุรี",
+lat:13.8700,
+lng:100.5000,
+map:"https://maps.app.goo.gl/Z1sPmeyPCAL6a36X8"
+},
+{
+name:"QUICKY วชิรธรรมสาธิต",
+lat:13.6800,
+lng:100.6400,
+map:"https://maps.app.goo.gl/BGXEcbNxaBB86U7V6"
+},
+{
+name:"QUICKY เฉลิมพระเกียรติ ร.9",
+lat:13.7000,
+lng:100.6900,
+map:"https://maps.app.goo.gl/AAKwALV9ZPG9PSWR7"
+},
+{
+name:"QUICKY ศรีนครินทร์38",
+lat:13.6900,
+lng:100.6500,
+map:"https://maps.app.goo.gl/pWk4A26Hn2GT5PZm9"
+},
+{
+name:"QUICKY สนามบินน้ำ",
+lat:13.8700,
+lng:100.5100,
+map:"https://maps.app.goo.gl/B74GFmj7e8smGvFV7"
+},
+{
+name:"QUICKY พระราม9",
+lat:13.7500,
+lng:100.6100,
+map:"https://maps.app.goo.gl/v91VVdnwQF96hrMh6"
+},
+{
+name:"QUICKY บางบอน (เอกชัย92)",
+lat:13.6500,
+lng:100.3800,
+map:"https://maps.app.goo.gl/4hodzhtqjmEk7HB27"
+},
+{
+name:"QUICKY ศรีนครินทร์-ลาซาล",
+lat:13.6700,
+lng:100.6400,
+map:"https://maps.app.goo.gl/ibfgwCZEowkTrH2U8"
+},
+{
+name:"QUICKY เกษตร-นวมินทร์",
+lat:13.8400,
+lng:100.6400,
+map:"https://maps.app.goo.gl/Xe9Ck4ac3bXhGWyJ9"
+},
+{
+name:"QUICKY กัลปพฤกษ์",
+lat:13.6900,
+lng:100.4300,
+map:"https://maps.app.goo.gl/VsZeBhVsUiMGBNX2A"
+}
+];
